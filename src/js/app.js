99b0c76315c60790793c73e63b28cc82b698b160
VMoiseev <e175@inbox.ru>
2022-08-26 20:35:14 +0500

    v1

--- src/js/app.js
+++ src/js/app.js
@@ -1,87 +1,60 @@
-const lists = document.querySelectorAll('.list');
-
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/* eslint-disable no-loop-func */
+/* eslint-disable no-plusplus */
 function addTask() {
-  const btn = document.querySelector('.add__btn'); // Кнопка add another card
-  const addBtn = document.querySelector('.add__item-btn'); // Кнопка add card под формой для заполнения
-  const canselBtn = document.querySelector('.cansel__item-btn'); // Кнопка крестик
-  const textarea = document.querySelector('.textarea'); // поле для заполнения
-  const form = document.querySelector('.form');
+  const btns = document.querySelectorAll('.add__btn'); // Кнопка add another card
+  const addBtns = document.querySelectorAll('.add__item-btn'); // Кнопка add card под формой для заполнения
+  const canselBtns = document.querySelectorAll('.cansel__item-btn'); // Кнопка крестик
+  // const textareas = document.querySelectorAll('.textarea');  // поле для заполнения
 
   let value;
 
-  btn.addEventListener('click', () => {
-    form.style.display = 'block';
-    btn.style.display = 'none';
-    textarea.addEventListener('input', (e) => {
-      value = e.target.value;
+  for (let i = 0; i < btns.length; i++) {
+    const changedBtn = btns[i];
+    changedBtn.addEventListener('click', (e) => {
+      changedBtn.style.display = 'none';
+      console.log('working');
+      const form = e.target.closest('.boards__item').querySelector('.form'); // Поднялся до ближ.общего предка и нашёл нужный элемент
+      form.style.display = 'block';
     });
-  });
 
-  canselBtn.addEventListener('click', () => {
-    textarea.value = '';
-    value = '';
-    form.style.display = 'none';
-    btn.style.display = 'block';
-  });
+    for (let a = 0; a < canselBtns.length; a++) {
+      const changedCanselBtn = canselBtns[a];
+      changedCanselBtn.addEventListener('click', (e) => {
+        const textarea = e.target.closest('.form');
+        textarea.value = '';
+        value = '';
+        const form = e.target.closest('.boards__item').querySelector('.form');
+        form.style.display = 'none';
+        changedBtn.style.display = 'block';
+      });
+    }
 
-  addBtn.addEventListener('click', () => {
-    const newItem = document.createElement('div');
-    newItem.classList.add('list__item');
-    newItem.draggable = true;
-    newItem.textContent = value;
-    lists[0].append(newItem);
+    for (b = 0; b < addBtns.length; b++) {
+      const changedAddBtn = addBtns[b];
+      // Кликая по кнопке Add Card должна создаться карточка
+      changedAddBtn.addEventListener('click', (e) => {
+        const newItem = document.createElement('div');
+        newItem.classList.add('list__item');
+        newItem.draggable = true;
+        const textarea = e.target.closest('.form');
+        // eslint-disable-next-line no-shadow
+        textarea.addEventListener('input', (e) => {
+          value = e.target.value;
+        });
+        newItem.textContent = value;
+        const lists = document.querySelectorAll('.list');
+        lists[0].append(newItem);
 
-    textarea.value = '';
-    value = '';
-    form.style.display = 'none';
-    btn.style.display = 'block';
-  });
+        // перед закрытием формы она должна очиститься
+        textarea.value = '';
+        value = '';
+        const form = e.target.closest('.boards__item').querySelector('.form');
+        form.style.display = 'none';
+        changedBtn.style.display = 'block';
+      });
+    }
+  }
 }
 addTask();
-
-// let draggedItem = null;
-
-// function dragNdrop() {
-//   const listItems = document.querySelectorAll('.list__item');
-//   const lists = document.querySelectorAll('.list');
-
-//   // eslint-disable-next-line no-plusplus
-//   for (let i = 0; i < listItems.length; i++) {
-//     const item = listItems[i];
-
-//     item.addEventListener('dragstart', () => {
-//       draggedItem = item;
-//       setTimeout(() => {
-//         item.style.display = 'none';
-//       }, 0);
-//     });
-
-//     item.addEventListener('dragend', () => {
-//       setTimeout(() => {
-//         item.style.display = 'block';
-//         draggedItem = null;
-//       }, 0);
-//     });
-//   }
-
-//   for (let j = 0; j < lists.length; j++) {
-//     const list = lists[j];
-
-//     list.addEventListener('dragover', (e) => e.preventDefault());
-
-//     list.addEventListener('dragenter', function (e) {
-//       e.preventDefault();
-//       this.style.backgroundColor = '#a19f9f';
-//     });
-
-//     list.addEventListener('dragleave', function (e) {
-//       this.style.backgroundColor = '#c0c0c0';
-//     });
-
-//     list.addEventListener('drop', function (e) {
-//       this.style.backgroundColor = '#c0c0c0';
-//       this.append(draggedItem);
-//     });
-//   }
-// }
-// dragNdrop();
